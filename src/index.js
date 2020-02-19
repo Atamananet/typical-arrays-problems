2af36b57b36173ec8f06c7b334accbe25c9cb79b
Atamananet <storozhenko.vladimir.vasilhevich@gmail.com>
2020-02-19 19:37:56 +0300

    feat: complete typical array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0){ 
+    return 0;
+  }
+  return array.reduce((accum, curr) => ( accum = (accum > curr) ? curr : accum),array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0){ 
+        return 0;
+  }
+  return array.reduce((accum, curr) => ( accum = (accum < curr) ? curr : accum),array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0){ 
+        return 0;
+  }
+
+  return array.reduce((accum, curr) => (accum += curr),0) / array.length;
 }
